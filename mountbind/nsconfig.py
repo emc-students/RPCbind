fdc92753c4f8e6dc42f11dcbd281de28a32beac8
kseniya-buraya <ksu.nyaka@gmail.com>
2015-04-15 01:30:29 +0300

    Modified python script but it doesn't work well somehow

--- mountbind/nsconfig.py
+++ mountbind/nsconfig.py
@@ -4,37 +4,57 @@ from pyroute2 import netns
 import os
 import re
 import sys
+import signal
+from subprocess import check_output
 
+def get_pid(name):
+    return int(check_output(["pidof","-s",name]))
 
-#if os.geteuid() != 0:
-#    print "This script must be run as root\nBye"
-#    exit(1)
-
-
-if (len(sys.argv) == 2) :
+if (len(sys.argv) == 3):
 	nsname = sys.argv[1]
+	status = sys.argv[2]
 else: 
-	print ("Please, check your input. Correct using is nsconfig.py NSname")
+	print ("Script usage: ./nsconfig.sh [NSname] [start/stop]")
+	sys.exit(1)
+
+lockfilename = nsname + ".lock"
+sockfilename = nsname + ".sock"
+pidfilename = nsname + ".pid"
+
+if (status == "start"):
+	file = open('/tmp/' + lockfilename, 'w+')
+	file.close()
+	file = open('/tmp/' + sockfilename, 'w+')
+	file.close()
+	file = open('/tmp/' + pidfilename, 'w+')
+	file.close()
+
+	bashcommand = "ip netns exec " + '"' + nsname + '"' + " mount --bind " + "/tmp/" + lockfilename + " /var/run/rpcbind.lock"
+	os.system(bashcommand)
+	bashcommand = "ip netns exec " + '"' + nsname + '"' + " mount --bind " + "/tmp/" + sockfilename + " /var/run/rpcbind.sock"
+	os.system(bashcommand)
+	bashcommand = "ip netns exec " + '"' + nsname + '"' + " rpcbind"
+	os.system(bashcommand)
+	pid = get_pid("rpcbind")
+	f = open('/tmp/' + pidfilename, 'w+')
+	f.write(str(pid))
+	f.close()
+
+elif (status == "stop"):
+	f = open('/tmp/' + pidfilename, 'w+')
+	pid = f.readline()
+	os.kill(int(pid), signal.SIGKILL)
+	bashcommand = "umount " + "/tmp/" + lockfilename
+	os.system(bashcommand)
+	bashcommand = "umount " + "/tmp/" + sockfilename
+	os.system(sockfile)
+	os.remove('/tmp/' + lockfilename)
+	os.remove('/tmp/' + sockfilename)
+	os.remove('/tmp/' + pidfilename)
+
+else:
+	print ("Script usage: ./nsconfig.sh [NSname] [start/stop]")
 	sys.exit(1)
 
 
-print(netns.listnetns())
-
-netnslist = os.listdir('/var/run/netns')
-flag = 0
-for netn in netnslist:
-	if(nsname == netn):
-		flag = 1
-#Adding namespase + NSname.lock file
-if(flag == 0):
-	netns.create(nsname)
-
-print(netns.listnetns())
-lockfilename = nsname + ".lock"
-file = open(lockfilename, 'w+')
-
-bashcommand = "ip netns exec " + nsname + " mount --bind " + lockfilename + " /var/run/rpcbind.lock"
-os.system(bashcommand)
-bashcommand = "ip netns exec " + nsname + " rpcbind"
-os.system(bashcommand)
 
